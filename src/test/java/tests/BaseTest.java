c84136a871d9e792879ab9b53b7e4284884cc10b
Saurabh Kumar <saurabhnitk13@gmail.com>
2025-05-25 23:45:24 +0530

    added maven surefire plugin

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,13 +1,17 @@
 package tests;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -23,12 +27,28 @@ public class BaseTest {
 	public Login login; 
 	public HomePage homepage;
 	public PatientsPage patientPage;
+	public String browserName;
 
 	@BeforeClass
-	public void setUp() {
-    	WebDriverManager.chromedriver().setup();
-    	driver = new ChromeDriver();
-    	driver.manage().window().maximize();
+	public void setUp() throws IOException {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//test//java//resources//GlobalData.properties");
+		prop.load(fis);
+		if(System.getProperty("browser") != null) {
+			browserName = System.getProperty("browser");
+			
+		}else {
+			browserName = prop.getProperty("browser"); 
+		}
+		if(browserName.equalsIgnoreCase("chrome")) {
+			//WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if(browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}else if(browserName.equalsIgnoreCase("firefox")) {
+			driver = new EdgeDriver();
+		}
+       	driver.manage().window().maximize();
     	driver.get("https://gor-pathology.web.app/");
     	login = new Login(driver);
     	homepage = new HomePage(driver);
